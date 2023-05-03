99428178c44b180b340069bc5f3a24ed122b4f63
Ben Myers <benl.myers01@gmail.com>
2023-05-03 00:21:47 -0700

    🔨 Start of ExpressionBuilder

--- src/main/java/com/dezeus/delta/lang/ExpressionBuilder.java
+++ src/main/java/com/dezeus/delta/lang/ExpressionBuilder.java
@@ -17,14 +17,18 @@ public class ExpressionBuilder {
     public Expression build(String s) throws InvalidExpressionException {
         List<Symbol> list = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ')
+                continue;
             for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(j) == ' ')
+                    continue;
                 String sub = s.substring(i, j);
                 try {
                     if (language.getSymbol(sub) != null) {
                         list.add(language.getSymbol(sub));
                     }
                 } catch (NoSymbolException _0) {
-                    // Do nothing
+                    throw new InvalidExpressionException(s);
                 }
             }
         }
